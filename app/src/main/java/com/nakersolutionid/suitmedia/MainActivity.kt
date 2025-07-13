76ea442d1336d2283a1b4131c62304850d702fa1
ts0ra <ts0ra354@proton.me>
2025-07-13 15:08:21 +0700

    feat: make nav root as initial screen

--- app/src/main/java/com/nakersolutionid/suitmedia/MainActivity.kt
+++ app/src/main/java/com/nakersolutionid/suitmedia/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.nakersolutionid.suitmedia.ui.navigation.NavigationRoot
 import com.nakersolutionid.suitmedia.ui.theme.SuitmediaTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SuitmediaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NavigationRoot()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SuitmediaTheme {
-        Greeting("Android")
     }
 }
