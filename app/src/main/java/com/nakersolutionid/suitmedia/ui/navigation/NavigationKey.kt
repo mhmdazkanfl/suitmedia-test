0301fadb226f882879f7b7fc5d34c36471da47f5
ts0ra <ts0ra354@proton.me>
2025-07-13 15:01:41 +0700

    feat: added nav root

---
+++ app/src/main/java/com/nakersolutionid/suitmedia/ui/navigation/NavigationKey.kt
@@ -0,0 +1,8 @@
+package com.nakersolutionid.suitmedia.ui.navigation
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable data object Palindrome : NavKey
+@Serializable data object Welcome : NavKey
+@Serializable data object UserSelect : NavKey
---
+++ app/src/main/java/com/nakersolutionid/suitmedia/ui/navigation/NavigationRoot.kt
@@ -0,0 +1,33 @@
+package com.nakersolutionid.suitmedia.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
+import androidx.navigation3.runtime.entry
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.runtime.rememberSavedStateNavEntryDecorator
+import androidx.navigation3.ui.NavDisplay
+import androidx.navigation3.ui.rememberSceneSetupNavEntryDecorator
+
+@Composable
+fun NavigationRoot(modifier: Modifier = Modifier) {
+    val backStack = rememberNavBackStack(Palindrome)
+
+    NavDisplay(
+        modifier = modifier,
+        backStack = backStack,
+        onBack = { backStack.removeLastOrNull() },
+        entryDecorators = listOf(
+            rememberSceneSetupNavEntryDecorator(),
+            rememberSavedStateNavEntryDecorator(),
+            rememberViewModelStoreNavEntryDecorator()
+        ),
+        entryProvider = entryProvider {
+            entry<Palindrome> { }
+            entry<Welcome> { }
+            entry<UserSelect> { }
+        }
+    )
+}
